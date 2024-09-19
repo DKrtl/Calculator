273dff05dbf8b1f172637e797fe8606af89e17ec
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2024-09-19 02:22:43 +0100

    implement appendToDisplay(), clearDisplay() and calculate() functions

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hello, JavaScript!');
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        stringEq = convertStringEquation(display.value);
+
+        display.value = eval(stringEq);
+    } catch (error) {
+        display.value = error;
+    }
+}
+
+function convertStringEquation(eq) {
+    return eq.replace(/÷/g, '/').replace(/×/g, '*');
+}
